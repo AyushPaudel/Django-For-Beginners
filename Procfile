de01c55e65a503f3e8b3d2f62efed4ee7383ad76
AyushPaudel <ap40021319@student.ku.edu.np>
2021-04-11 21:54:06 +0545

    New updates for Heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn helloworld_project.wsgi --log-file -
--- helloworld_project/settings.py
+++ helloworld_project/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-z3m6bb)6vs8l-xo^d8r)-mqat(05xn(=__sa%#wx#4oc5@h146
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
