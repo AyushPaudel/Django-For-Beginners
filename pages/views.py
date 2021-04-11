320110808d447b6895b792ce466173105acdf807
AyushPaudel <ap40021319@student.ku.edu.np>
2021-04-11 21:35:54 +0545

    templates, test cases added

--- pages/views.py
+++ pages/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.views.generic import TemplateView
 
-def homePageView(request):
-    return HttpResponse('Hello, World!')
+class HomePageView(TemplateView):
+    template_name = 'home.html'
+
+class AboutPageView(TemplateView):
+    template_name = 'about.html'
