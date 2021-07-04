092c77b5b1cef64aa0d3a97e614a5a62a7a6ad8c
AyushPaudel <ap40021319@student.ku.edu.np>
2021-07-04 22:24:24 +0545

    pushed chapter 4

--- 4/posts/models.py
+++ 4/posts/models.py
@@ -1,3 +1,10 @@
+# ayushoo7 :admin123@
+
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+    text = models.TextField()
+
+    def __str__(self):
+        return self.text[:50]
+
